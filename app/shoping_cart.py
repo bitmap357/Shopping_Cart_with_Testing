c91b2fc42e5605cab242b318441569422e72a7ca
bitmap357 <edakoyeram@gmail.com>
2023-07-27 22:39:07 +0000

    add item and remove item

--- app/shoping_cart.py
+++ app/shoping_cart.py
@@ -1,3 +1,15 @@
 class ShoppingCart:
     def __init__(self):
         self.items = {}
+
+    def add_item(self, item, quantity):
+        if item in self.items:
+            self.items[item] += quantity
+        else:
+            self.items[item] = quantity
+
+    def remove_item(self, item, quantity):
+        if quantity >= self.items[item]:
+            del self.items[item]
+        else:
+            self.items[item] -= quantity
