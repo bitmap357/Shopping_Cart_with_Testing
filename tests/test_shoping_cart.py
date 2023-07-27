468871ad7da4158db79755a420777ace2c7b937c
bitmap357 <edakoyeram@gmail.com>
2023-07-27 22:49:46 +0000

    testing get cart items

--- tests/test_shoping_cart.py
+++ tests/test_shoping_cart.py
@@ -15,3 +15,12 @@ def test_remove_item():
     cart.remove_item("apple", 2)
     assert cart.get_item_count("apple") == 1
     assert cart.get_total_items() == 1
+
+
+def test_get_cart_items():
+    cart = ShoppingCart()
+    cart.add_item("apple", 2)
+    cart.add_item("banana", 3)
+    items = cart.get_cart_items()
+    assert "apple" in items
+    assert "banana" in items
